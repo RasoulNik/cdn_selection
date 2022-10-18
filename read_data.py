e64874c32821a5ae817b685bfbb55dcc3890a1a4
rnikbakht <asliMAIL1989>
2022-10-18 13:03:12 +0200

    commit the database

---
+++ read_data.py
@@ -0,0 +1,21 @@
+import csv
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+#
+# file = open('5gsolutions-dataset-uc4-2.csv')
+# csvreader = csv.reader(file)
+# rows = []
+# for row in csvreader:
+#         rows.append(row)
+# data = []
+# for row in rows[1:]:
+#         data
+# data = np.array(rows[1:])
+# np.mean(data)
+data= pd.read_csv('102-edge-final.csv')
+
+bw = data.get("bw")
+
+np.mean(bw)
+np.std(bw)
---
+++ readme.md
@@ -0,0 +1,5 @@
+Data source
+https://zenodo.org/record/3459164
+
+Fed4Fire/CDN-X-ALL network metrics dataset for time series analysis in Media content delivery for 4G/5G networks
+https://datasetsearch.research.google.com/search?src=2&query=Fed4Fire%2FCDN-X-ALL%20network%20metrics%20dataset%20for%20time%20series%20analysis%20in%20Media%20content%20delivery%20for%204G%2F5G%20networks&docid=L2cvMTFqbl8xa3JieA%3D%3D
